f2015e7c60a29b8d4864389848d4917ab5aad2a2
Callum <cfa5228@psu.edu>
2022-10-31 14:08:59 -0400

    Put code in functions, use rel paths, correct distance calculation
    Distance calculation incorrectly used previous distance when no movement was recorded.
    
    Wrap in functions and write csv to new file, rather than renaming a copy of the csv before manipulating. This also makes it easy to use the timestamp recorded in the csv file, which is corrected for the timezone.
    
    Use `{pyprojroot}` package for relative paths

--- test-activities/clean-painsled-csv.py
+++ test-activities/clean-painsled-csv.py
@@ -17,9 +17,13 @@ import csv
 import datatable as dt
 from datatable import *
 from numpy import NaN
+from pyprojroot import here
+from urllib3 import Retry
 
 #%%
-activity_dir = os.listdir()
+input_dir_path = str(here("test-activities/original-files"))
+activity_dir = os.listdir(input_dir_path)
+output_dir_path = str(here("test-activities/cleaned-files"))
 
 #%%
 def rename_painsled_csv(file):
@@ -44,28 +48,105 @@ def rename_painsled_csv(file):
         else:
             error("Error: filename does not match expected format")
 
-
-#%%
-for file in activity_dir:
-    if file.endswith(".csv"):
-        rename_painsled_csv(file)
-
-#%%
-# Reset directory pointer as file names have changed
-activity_dir = os.listdir()
-
-#%%
-for file in activity_dir:
-    if file.endswith(".csv"):
-        DT = dt.fread(file)
-        print(DT[:, {"Elapsed Time (sec)": "Time"}])
-
-
-# xrscrn_20221018T220042261Z.csv
-# xrscrn_20221026T221847640Z.csv
-# xrscrn_20221027T222030379Z.csv
 # %%
 test_file = dt.fread("xrscrn_20221018T220042261Z.csv")
+
+#%%
+def rename_painsled_csv_vars(file_name, input_dir_path):
+    file = input_dir_path + "/" + file_name
+    DT = dt.fread(file)
+    
+    DT.names = {
+        "ElapsedTime (sec)": "Seconds",
+        "Horizontal (meters)" : "Meters",
+        "Power (watts)" : "Watts",
+        "Cadence (strokes/min)" : "Cadence",
+        "HRCur (bpm)" : "Hrate"
+    }
+
+    return DT
+
+def feature_painsled_vars(DT):
+    DT[:, dt.update(**{
+        "Torq (N-m)" : None,
+        "s_500m" : ifelse(f.Watts == 0, 0, 500*(2.8/f.Watts)**(1/3)),
+        "ID" : 0,
+        "Calc_stroke_dist_m" : ifelse( # Takes multiple conditions
+            shift(f.Meters) == None, f.Meters,
+            f.Meters - shift(f.Meters) < 0, f.Meters,
+            f.Meters - shift(f.Meters)
+        ),
+        "Calc_time_diff_sec": ifelse(
+            shift(f["TimeStamp (sec utc)"]) == None,
+            f["Seconds"],
+            f["TimeStamp (sec utc)"] - shift(f["TimeStamp (sec utc)"])
+        )
+        })]
+        
+    DT[:, dt.update(**{
+        "m_s" : ifelse(f.Watts == 0, 0, 500/f.s_500m),
+        "Cum_dist_m" : f["Calc_stroke_dist_m"].cumsum(),
+        "Cum_time_sec" : f["Calc_time_diff_sec"].cumsum()
+        })]
+    DT[:, dt.update(**{
+        "Km/h" : ifelse(f.Watts == 0, 0, f.m_s*3.6),
+        "Km" : f.Cum_dist_m/1000,
+        "Minutes" : f.Cum_time_sec/60
+        })]
+
+    DT = DT[:, ["Minutes", "Torq (N-m)","Km/h","Watts","Km","Cadence","Hrate","ID", "StrokeCount"]]
+
+    return DT
+
+
+def correct_painsled_lap_num(DT):
+    for row in range(0, DT.shape[0]):
+        if row == 0 and DT[row, "StrokeCount"] == 1:
+            if DT[99, "StrokeCount"] > 98:
+                DT[row, "ID"] = 1
+            else:
+                DT[row, "ID"] = 0
+                continue
+
+        elif row == 0 and DT[row, "StrokeCount"] == 0:
+            DT[row, "ID"] = 1
+            continue
+
+        elif row != 0 and DT[row, "StrokeCount"] == 0:
+            DT[row, "ID"] = DT[row-1, "ID"] + 1
+            continue
+        elif row != 0 and DT[row, "StrokeCount"] == 1:
+            DT[row, "ID"] = DT[row-1, "ID"] + 1
+            continue
+        elif row != 0 and DT[row, "StrokeCount"] > 1:
+            DT[row, "ID"] = DT[row-1, "ID"]
+            continue
+        elif row == len(PD) - 1 and DT[row, "StrokeCount"] == 0:
+            DT[row, "ID"] = DT[row-1, "ID"] + 1
+    
+    return DT
+
+def correct_painsled_filename(DT):
+    datetime_obj = datetime.fromtimestamp(DT[0, "TimeStamp (sec utc)"])
+    filename_str = datetime_obj.strftime("%Y_%m_%d_%H_%M_%S")
+
+    return filename_str
+
+#%%
+def clean_painsled_csv(csv_file, input_dir_path, output_dir_path):
+    renamed_DT = rename_painsled_csv_vars(csv_file, str(input_dir_path))
+    featured_DT = feature_painsled_vars(renamed_DT)
+    output_DT = correct_painsled_lap_num(featured_DT)
+    filename_str = correct_painsled_filename(renamed_DT)
+    filename_path = output_dir_path + "/" + filename_str + ".csv"
+
+    output_DT.to_csv(filename_path)
+
+#%%
+for file in activity_dir:
+    if file.startswith("xrscrn_") and file.endswith(".csv"):
+        test_DT = clean_painsled_csv(file, input_dir_path, output_dir_path)
+
 
 #%%
 # Minutes,Torq (N-m),Km/h,Watts,Km,Cadence,Hrate,ID
