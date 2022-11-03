32cba3575e4d9326f6c2e5719e0538cf1a9e028c
Callum <cfa5228@psu.edu>
2022-11-03 12:57:35 -0400

    Use HRV4Training which has cleaner export and better signal processing

--- import-script.sh
+++ import-script.sh
@@ -4,5 +4,6 @@ garmin-backup --backup-dir=activities --format=fit $1 --password=$2
 
 cp ~/Google\ Drive/My\ Drive/Painsled/*.tcx ./painsled-activities/original-files/
 cp ~/Google\ Drive/My\ Drive/Painsled/*.csv ./painsled-activities/original-files/
+cp ~/Dropbox/Apps/HRV4Training/*.csv ./hrv4training/
 
 python3 clean-painsled-csv.py
